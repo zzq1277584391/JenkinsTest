5acace26a880421ab1b5a793a539b9c57d36e57f
pikachu <1277584391@qq.com>
2025-01-31 15:56:31 +0800

    Test

--- Main.groovy
+++ Main.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo 'Hello zzq!'
+                echo 'Hello zzq, this is branch'
             }
         }
     }
