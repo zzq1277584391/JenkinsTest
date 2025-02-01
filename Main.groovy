9219e76a5c431e2da17ff4ca94259e204c40b4b5
pikachu <1277584391@qq.com>
2025-02-01 22:31:24 +0800

    Test

--- Main.groovy
+++ Main.groovy
@@ -4,10 +4,13 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                def command = 'cmd /c dir' // 通过 cmd 执行 dir 命令
-                def process = command.execute() // 执行命令
-                def output = process.text.trim() // 获取输出
-                echo "Command output: ${output}"            }
+                script {
+                    def command = 'cmd /c dir' // 通过 cmd 执行 dir 命令
+                    def process = command.execute() // 执行命令
+                    def output = process.text.trim() // 获取输出
+                    echo "Command output: ${output}"           
+                }
+            }
         }
     }
 }
